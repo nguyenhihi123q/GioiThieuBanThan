f5a70d67bb9cc73c14556cd80ba8dc9605da78a7
Trần Võ Hoàng Nguyên <haringnguyen5@gmail.com>
2026-07-16 22:15:08 +0700

    chore: redeploy after pages source update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/ - trigger deploy to github actions
+// https://vite.dev/config/ - redeploy after pages source update
 export default defineConfig({
   base: '/GioiThieuBanThan/',
   plugins: [react()],
