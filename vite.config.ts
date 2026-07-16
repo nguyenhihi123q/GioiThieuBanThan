e3598937e7e41c3d830c33d699caf6f116dabaa5
Trần Võ Hoàng Nguyên <haringnguyen5@gmail.com>
2026-07-16 21:51:56 +0700

    chore: trigger deploy on github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ - trigger deploy to github actions
 export default defineConfig({
   base: '/GioiThieuBanThan/',
   plugins: [react()],
